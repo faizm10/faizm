8f973d1c6ebe691048c020651694e0a664355624
faizm10 <faizmustansar10@gmail.com>
2026-01-18 13:25:14 -0500

    feat: added arcki project

--- app/projects.ts
+++ app/projects.ts
@@ -26,4 +26,13 @@ export const showcaseProjects: ProjectType[] = [
     glowColors:
       "drop-shadow(0 8px 16px rgba(204, 85, 0, 0.04)) drop-shadow(0 12px 24px rgba(230, 115, 40, 0.04)) drop-shadow(0 16px 32px rgba(255, 140, 0, 0.03)) drop-shadow(0 20px 40px rgba(255, 165, 50, 0.03))",
   },
+  {
+    name: "arcki",
+    slug: "arcki",
+    url: "https://www.arcki.tech/",
+    banner: "/projects/banners/arcki.png",
+    desc: "architect the world the way you imagine it",
+    glowColors:
+      "drop-shadow(0 8px 16px rgba(204, 85, 0, 0.04)) drop-shadow(0 12px 24px rgba(230, 115, 40, 0.04)) drop-shadow(0 16px 32px rgba(255, 140, 0, 0.03)) drop-shadow(0 20px 40px rgba(255, 165, 50, 0.03))",
+  },
 ];
